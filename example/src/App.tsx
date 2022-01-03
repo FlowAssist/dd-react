8e2433bf8048abb81bb3b478cd6c6b49cae6f474
Krit Bannachaisirisuk <gmkipladen@gmail.com>
2022-01-03 23:11:43 +0700

    initialize sample npm module

---
+++ example/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SayHello from 'dd-react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SayHello name='sample' />
+    </div>
+  );
+}
+
+export default App;
